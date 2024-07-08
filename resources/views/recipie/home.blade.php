9401423f92ade6ec4af1234b04987cde0e33cf88
Malik <vofene3689@wlmycn.com>
2024-07-08 18:22:58 +0500

    fixes

--- resources/views/recipie/home.blade.php
+++ resources/views/recipie/home.blade.php
@@ -4,9 +4,9 @@
 <!-- Image and text -->
         <div class="relative w-full">
             <img src="/img/image1.jpg" alt="" class="h-auto w-full">
-                <span class="text-7xl text-white top-[40%] left-[20%] absolute font-extrabold">Lets</span>
-                <h1 class="text-7xl text-white absolute top-[50%] left-[28%] font-extrabold" >Enjoy our </h1>
-                <span class="text-white text-7xl top-[60%] left-[35%] absolute font-extrabold">Delicius Recipies</span>
+                <span class="text-8xl text-white top-[35%] left-[20%] absolute font-extrabold">Lets</span>
+                <h1 class="text-8xl text-white absolute top-[47%] left-[28%] font-extrabold" >Enjoy our </h1>
+                <span class="text-white text-8xl top-[60%] left-[35%] absolute font-extrabold">Delicius Recipies</span>
         </div>
         <!-- Category section -->
                 <p class="text-center text-orange-600 font-light text-xl mt-4">Choose a Category</p>
@@ -30,7 +30,7 @@
                 <h1 class="text-2xl font-bold my-3 px-8">Title</h1>
                 @include('partials._ratings')
                 <p class="text-sm  my-3 px-8 leading-6">A simple and easy to made recipie with minimal ingrediants and refreshing and good pricing. </p>
-                
+
 
             </div>
         </div>
