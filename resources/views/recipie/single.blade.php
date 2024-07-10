ce60bd07cdeee1ff51b72ba439373b30963b03c4
Malik <vofene3689@wlmycn.com>
2024-07-10 17:37:27 +0500

    UI settings of Single recipie view

--- resources/views/recipie/single.blade.php
+++ resources/views/recipie/single.blade.php
@@ -1,38 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-center text-2xl font-semibold my-3">{{$recipie->name}}</h1>
-<div class="grid grid-cols-2 gap-4 w-[90%] my-4 mx-auto h-auto">
-    <div class="bg-slate-50 h-full w-full p-4" >
-        <img src="{{asset('storage/'.$recipie->picture)}}" alt="" >
+<h1 class="text-center text-6xl my-3 font-extrabold border-t border-gray-200 py-1">{{$recipie->name}}</h1>
+ <div class="flex items-center justify-center text-2xl flex-col">
+    <h2 class="text-xl font-light text-lime-500 ">{{$recipie->category}}</h2>
+    @include('partials._ratings')
+ </div> 
+    <div class="w-[70%] mx-auto flex item-center justify-center h-[500px] overflow-hidden rounded-lg shadow-md">
+    <img src="{{asset('storage/'.$recipie->picture)}}" alt="" class="w-full hover:scale-105 transition-all duration-500 ease-in-out">
     </div>
-    <div class="bg-slate-50">
-        <h1 class="text-center text-xl font-semibold my-3"> Category: {{$recipie->category}}</h1>
-        
-{{-- Rating code  --}}
-<div class="flex items-center justify-center">Rating 
-    <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-    </svg>
-    <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-    </svg>
-    <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-    </svg>
-    <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-    </svg>
-    <svg class="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-    </svg>
-</div>
-    
-        <h2 class="text-center font-bold my-2 text-xl">How to make it</h2>
-        <p class="p-2">
+    <div class="bg-slate-50 flex items-center justify-center flex-col">
+      <h1 class="text-2xl font-mono text-center py-4 ">Main Ingredients</h1>
+      <p class="text-center border-2 border-gray-100 w-[70%] mx-auto py-8">{{$recipie->ingredients}}</p>         
+        <h2 class="text-2xl font-mono text-center py-4">Instructions</h2>
+        <p class="text-center border-2 border-gray-100 w-[70%] mx-auto py-4">
            {{$recipie->instruction}}
         </p>
-
+        <a href="{{$recipie->link}}" target="_blank" 
+            class="bg-orange-600 px-16 py-3 text-white rounded-sm mx-auto my-4"><i class="fa-brands fa-youtube"></i> Watch</a>
     </div>
 </div>
 @endsection
